4cd4acf251070b80a71f6cd26fb240c2ddf4ab3d
uddt <uddt_ds@kakao.com>
2025-05-23 17:49:39 +0900

    [FEAT] #13 -UserDefaults Manager, UserDefaults Model 구현
    - 재사용이 가능한 UserDefaults Manager를 구현하였습니다.
    - 주소를 저장할 UserDefaults Model을 작성하였습니다.

---
+++ WeatherWhat/WeatherWhat/Model/LocationHistory.swift
@@ -0,0 +1,12 @@
+//
+//  LocationHistory.swift
+//  WeatherWhat
+//
+//  Created by Lee on 5/23/25.
+//
+
+import Foundation
+
+struct LocationHistory: Codable {
+    var history: [UserLocationData]
+}
