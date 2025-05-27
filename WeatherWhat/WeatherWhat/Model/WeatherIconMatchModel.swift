210c03f10c7ad76595c30f4a1c5c89a3d9a14b9a
LCH-1228 <chanho1228@gmail.com>
2025-05-28 00:52:09 +0900

    [Feat] #10 - 메인화면 View모델 구현

---
+++ WeatherWhat/WeatherWhat/Model/WeatherIconMatchModel.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherIconMatchModel.swift
+//  WeatherWhat
+//
+//  Created by LCH on 5/27/25.
+//
+
+struct WeatherIconMatchModel {
+    
+    let dictionary: [String : String] = [
+        "01d" : "pureWhite",
+        "01n" : "pureWhite",
+        "02d" : "pureWhite",
+        "02n" : "pureWhite",
+        "03d" : "pureWhite",
+        "03n" : "pureWhite",
+        "04d" : "rainyBackground",
+        "04n" : "rainyBackground",
+        "09d" : "rainyBackground",
+        "09n" : "rainyBackground",
+        "10d" : "rainyBackground",
+        "10n" : "rainyBackground",
+        "11d" : "rainyBackground",
+        "11n" : "rainyBackground",
+        "13d" : "pureWhite",
+        "13n" : "pureWhite",
+        "50d" : "pureWhite",
+        "50n" : "pureWhite"
+    ]
+}
